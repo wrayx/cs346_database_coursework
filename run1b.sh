3e1d21db97776dd5f5e22afb277a1fde8a7e3c0b
Niuyitongniu <1466009885@qq.com>
2022-02-28 14:45:34 +0000

    TopKItems.jar

---
+++ run1b.sh
@@ -0,0 +1,13 @@
+if [[ $1 == "rm" ]]
+then
+    hdfs dfs -rm -r output/q1b;
+elif [[ $1 == "cat" ]]
+then
+    hdfs dfs -cat output/q1b/part-r-00000;
+else
+    rm com/cs346id18/com/cs346id18/part3/q1b/Top*.class
+    rm TopKItems.jar
+    hadoop com.sun.tools.javac.Main com/cs346id18/part3/q1b/TopKItems.java
+    jar -cf TopKItems.jar com/cs346id18/part3/q1b/TopKItems*.class
+    hadoop jar TopKItems.jar com.cs346id18.part3.q1b.TopKItems 10 2451392 2451894 input/1G/store_sales/store_sales.dat output/q1b
+fi

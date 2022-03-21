712e4c44032a9ca7b43efc7476c44d2fed437def
Niuyitongniu <1466009885@qq.com>
2022-03-21 21:21:03 +0000

    m

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ if [[ $1 == "a" ]]; then
         rm TopKNetProfit.jar
         hadoop com.sun.tools.javac.Main com/cs346id18/part3/q1a/TopKNetProfit.java
         jar -cf TopKNetProfit.jar com/cs346id18/part3/q1a/TopKNetProfit*.class
-        hadoop jar TopKNetProfit.jar com.cs346id18.part3.q1a.TopKNetProfit 10 2451146 2452268 input/1G/store_sales/store_sales.dat output/q1a
+        hadoop jar TopKNetProfit.jar com.cs346id18.part3.q1a.TopKNetProfit 10 2451146 2452268 input/40G/store_sales/store_sales.dat output/q1a
     fi
 elif [[ $1 == "b" ]]; then
     if [[ $2 == "rm" ]]; then

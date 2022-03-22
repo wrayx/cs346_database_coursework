ecce39f10d011c802ec7eae605fd99cbed7e5995
leohe27 <hxc20001227@sina.com>
2022-03-22 20:58:45 +0000

    EDA

--- eda.py
+++ eda.py
@@ -14,45 +14,39 @@ df_store_sales = dd.read_csv("/dcs/cs346/tpcds/40G/store_sales.dat",
                              sep = '|',
                              names = store_sales_header_list,
                              usecols = store_sales_cols)
-print(df_store_sales.head(5))
 
 
-print('Shape of the dataset:')
-df_store_sales.shape
+store_cols = [0, 7]
+store_header_list = ["Store","Floor Space"]
+df_store = dd.read_csv("/dcs/cs346/tpcds/40G/store.dat",
+                        sep = '|',
+                        names = store_header_list,
+                        usecols = store_cols)
+
+# print('------------------------')
+# print('Shape of the dataset:')
+# df_store_sales.shape
+
+# print('------------------------')
+# print('Data types of the dataset: ')
+# df_store_sales.info()
 
 
-print('Data types of the dataset: ')
-df_store_sales.info()
+
+# print('Describle the dataset')
+# df_store_sales.describe().to_csv("store_sales_describe.csv", index=True)
+# df_store.describe().to_csv("store_describe.csv", index=True)
 
 
 
-print('Describle the dataset')
-df_store_sales.describe()
-
-print('Cardinality of the columns')
-for col in df_store_sales:
-    cardinality = len(pd.Index(df_store_sales[col]).value_counts())
-    print(df_store_sales[col].name + ": " + str(cardinality))
-
-
-# print('Counts of each store')
-# store_counts = df_store_sales['Store'].value_counts()
-# print(store_counts)
 
 # print('Visualize Missing value')
-# fig = mn.matrix(df_store_sales.head(500))
+# fig = mn.matrix(df_store.head(500))
 # fig_copy = fig.get_figure()
-# fig_copy.savefig('plots/matrix_plot.png', bbox_inches = 'tight')
+# fig_copy.savefig('plots/matrix_plot_store.png', bbox_inches = 'tight')
 
 
-# df_store_netpaid = df_store_sales.loc[:,['Store','Net Paid']]
-
-# df_final = df_store_netpaid.groupby('Store',as_index = False).sum()
-# df_final.head()
-# df_final.plot.bar(x='Store', y="Net Paid", rot=70, title="Net Paid of Stores")
-# plt.show(block=True)
-# plt.savefig('plots/bar_chart.png', bbox_inches='tight')
-
-# plt.figure(figsize=(20,10))
-# plt.hist(df_store_sales['Sold Date'], bins=175 ,edgecolor='black', density=1)
-# plt.show() 
+plt.figure(figsize=(20,10))
+plt.hist(df_store_sales['Sold Date'], bins=175 ,edgecolor='black', density=1)
+plt.show() 
+plt.savefig('plots/histogram.png', bbox_inches='tight')
